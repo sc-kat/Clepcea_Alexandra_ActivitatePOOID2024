f51fb1ae0e669359ee73e8bff2c3a838d5dd88e2
sckat <ala_cl@yahoo.com>
2024-10-17 16:35:42 +0300

    info intalnire 1

---
+++ intalnire1id.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+
+using namespace std;
+
+class Robot {
+public:
+	string nume;
+	int versiuneSoft;
+	float memorie;
+	bool esteBiped;
+	char* culoare;
+
+	//destructor
+	~Robot() {    
+		if (this->culoare != NULL) {
+			delete[]this->culoare;
+		}
+	}
+
+	Robot() {
+		this->nume = "Tesla";
+		this->memorie = 78.9;
+		this->versiuneSoft = 2;
+		this->esteBiped = true;
+		//robot1.culoare = (char*)malloc(strlen("Alb")+1);
+		this->culoare = new char[strlen("Alb") + 1];
+		strcpy_s(this->culoare, strlen("Alb") + 1, "Alb");
+	}
+
+	Robot(string nume) {
+		this->nume = nume;
+		this->memorie = 78.9;
+		this->versiuneSoft = 2;
+		this->esteBiped = true;
+		//robot1.culoare = (char*)malloc(strlen("Alb")+1);
+		this->culoare = new char[strlen("Alb") + 1];
+		strcpy_s(this->culoare, strlen("Alb") + 1, "Alb");
+	}
+
+	Robot(int versiuneSoft) {
+		this->nume = "Tesla";
+		this->memorie = 78.9;
+		this->versiuneSoft = versiuneSoft;
+		this->esteBiped = true;
+		//robot1.culoare = (char*)malloc(strlen("Alb")+1);
+		this->culoare = new char[strlen("Alb") + 1];
+		strcpy_s(this->culoare, strlen("Alb") + 1, "Alb");
+	}
+
+	Robot(string nume, int versiuneSoft, float memorie, bool esteBiped,const char* culoare) {
+		this->nume = nume;
+		this->memorie = memorie;
+		this->versiuneSoft = versiuneSoft;
+		this->esteBiped = esteBiped;
+		//robot1.culoare = (char*)malloc(strlen("Alb")+1);
+		this->culoare = new char[strlen(culoare) + 1];
+		strcpy_s(this->culoare, strlen(culoare) + 1, culoare);
+
+	}
+
+	void afisare() {
+		cout << "Nume:" << this->nume << endl; // -> este echivalent cu deferentiere plus accesare
+		cout << "Soft:" << this->versiuneSoft << endl; 
+		cout << "Memorie: " << this->memorie << endl; 
+		cout << "Culoare: " << this->culoare << endl; 
+		cout << "Este biped: " << (this->esteBiped? "DA":"NU") << endl;
+	}
+};
+
+void main(){
+	Robot robot1;
+	robot1.afisare();
+
+	Robot robot2("Marcel");
+	robot2.afisare();
+
+	Robot robot3(10);
+	robot3.afisare();
+
+	Robot robot4("Gigel", 200, 56.7, 0, "Rosu");
+	robot4.afisare();
+
+	Robot* pointer;
+	pointer = new Robot("Cornel");
+	delete pointer;
+}
