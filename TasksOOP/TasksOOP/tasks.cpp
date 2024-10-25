f9f51e58cfc25d0f9045971d877e375adef00459
sckat <ala_cl@yahoo.com>
2024-10-25 14:57:44 +0300

    faza1- creare 2 clase cu atribute

---
+++ TasksOOP/TasksOOP/tasks.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+class Pantalon {
+public:
+	const int id;
+	int marime;
+	static int nrPantaloni;
+	char* culoare;
+};
+
+class Bluza {
+	const int id;
+	int marime;
+	static int nrBluze;
+	char* culoare;
+};
+
+class Fusta {
+	const int id;
+	int marime;
+	static int nrFuste;
+	char* culoare;
+};
+
+int main() {
+
+}
